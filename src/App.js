92acbe2cda56d221f86f80a2d647222d5c949598
Uliana Cheklina <uche0790@gmail.com>
2023-01-22 13:56:09 +0100

    Add some useState

--- src/App.js
+++ src/App.js
@@ -1,88 +1,82 @@
+import { css } from '@emotion/react';
 // import { url } from 'inspector';
 import { useState } from 'react';
 
 export default function App() {
-  const [imgUrl, setImgUrl] = useState(
-    'https://api.memegen.link/images/leo/I_have/_no_clue/.png',
-  );
-  const [topText, setTopText] = useState(
-    'https://api.memegen.link/images/doge.png',
-  );
-  const [bottomText, setBottomText] = useState('');
-  function memeGenerator() {
-    return (
-      <img
-        alt="the meme with your text"
-        src={`https://api.memegen.link/images/leo/${topText}/${bottomText}.png`}
-      />
-    );
+  const dogeUrl = 'https://api.memegen.link/images/doge.png';
+  const aag = 'https://api.memegen.link/images/aag.png';
+  // to generate memes
+  const [imgUrl, setImgUrl] = useState(aag);
+  // to put text on aag pic onChange
+  const [topText, setTopText] = useState(aag);
+  const [bottomText, setBottomText] = useState(aag);
+
+  //  const [chosenTop, setChosenTop] = useState(doge);
+  // const [doge, setDoge] = useState(dogeUrl);
+  function textGenerator() {
+    if (topText && !bottomText) {
+      return (
+        <img
+          alt="meme with top text"
+          src={`https://api.memegen.link/images/${imgUrl}/${topText}.png`}
+        />
+      );
+    } else if (!topText && bottomText) {
+      return (
+        <img
+          alt="meme with bottom text"
+          src={`https://api.memegen.link/images/${imgUrl}/_/${bottomText}.png`}
+        />
+      );
+    } else if (topText && bottomText) {
+      return (
+        <img
+          alt="the meme with top and bottom text"
+          src={`https://api.memegen.link/images/${imgUrl}/${topText}/${bottomText}.png`}
+        />
+      );
+    }
   }
   console.log(topText, bottomText);
+  function generateMeme() {}
   return (
-    <div
-      style={{
-        height: '500vh',
-        margin: '00px 40px',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'flex-start',
-        backgroundRepeat: 'no-repeat',
-        // backgroundImage: url(imgUrl),
-        fontWeight: '800',
-        fontSize: '20px',
-        letterSpacing: '2px',
-        color: 'Black',
-      }}
-    >
+    <div>
+      <header
+        css={css`
+          background-color: blue;
+          padding: 32px;
+          font-size: 24px;
+        `}
+      >
+        <img
+          src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"
+          alt="Problem?"
+          width="60px"
+        />
+        <p>Meme Generator</p>
+      </header>
       <p>Top Text</p>
       <label htmlFor="Top text">
-        <input
-          onChange={() => setTopText(memeGenerator)}
-          value={topText}
-          style={{
-            alignItems: 'center',
-            borderRadius: '20px',
-          }}
-        />
+        <input onChange={() => setTopText(textGenerator)} value={topText} />
       </label>
-
       <p>Bottom text</p>
       <label htmlFor="Bottom text">
         <input
-          onChange={(e) => setBottomText(memeGenerator)}
+          onChange={(e) => setBottomText(textGenerator)}
           value={bottomText}
-          style={{ borderRadius: '20px' }}
         />
       </label>
-      <p>Meme template selector</p>
-      <select
-        style={{
-          margin: '32px 0',
-          borderRadius: '20px',
-        }}
-      >
-        Meme template selector
-      </select>
+      <p>Meme selector</p>
+      <select>Meme template selector</select>
 
       <button
-        style={{
-          padding: '5px 20px',
-          borderRadius: '20px',
-          backgroundColor: '#8d99ae',
-        }}
+        data-test-id="generate-meme"
+        onClick={(e) => setDoge(generateMeme)}
       >
         Generate
       </button>
       <br />
-      <button
-        style={{
-          padding: '5px 27px',
-          borderRadius: '20px',
-          backgroundColor: '#d90429',
-        }}
-      >
-        Submit
-      </button>
+      <button>Download</button>
       <div>
         <img alt="your meme will be here" src={imgUrl} />
       </div>
